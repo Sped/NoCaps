47c4aeb32a9ae515618344165cc5192f0b51fd7f
Whattehp406 <bjornverstrepen@msn.com>
2012-07-02 19:30:59 +0200

    Added more comments explaining what the code does

--- src/com/tekkitcraft/sped/nocaps/NoCaps.java
+++ src/com/tekkitcraft/sped/nocaps/NoCaps.java
@@ -23,9 +23,8 @@ public class NoCaps extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable(){ 
-		getServer().getPluginManager().registerEvents(this, this);
-		log = this.getLogger();
-		loadConfiguration();
+		getServer().getPluginManager().registerEvents(this, this); //register the plugin to receive events
+		loadConfiguration(); //load config
 		log.info("NoCaps enabled!");
 	}
 	
@@ -34,12 +33,12 @@ public class NoCaps extends JavaPlugin implements Listener {
 		log.info("NoCaps disabled!");
 	}
 	
-	public void loadConfiguration() {//to load config file
-        config = getConfig();//get the configuration
+	public void loadConfiguration() { //to load config file
+        config = getConfig(); //get the configuration
 
-        addDefault("msg.caps", "§4Please don't caps :-(");//add and set default
-        addDefault("msg.minimumlenght", "5");
-        addDefault("command.executeoncaps", "/warn <sender> NoCaps: Caps");
+        addDefault("msg.caps", "§4Please don't caps :-("); //add and set default
+        addDefault("msg.minimumlenght", "5"); //Minimumlength to allow caps, to prevent "LOL" and small caps words from being detected as full caps
+        addDefault("command.executeoncaps", "/warn <sender> NoCaps: Caps"); //command to execute on a full caps
         addDefault("command.enableexecution", "false"); //Defaults to false
 
         config.options().copyDefaults(true);//copy the default from above
@@ -54,16 +53,16 @@ public class NoCaps extends JavaPlugin implements Listener {
         }
     }
 	
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST) //high priority since it needs to edit the message sent
 	public void onPlayerChat(PlayerChatEvent event) { //called when player chats.
 		String msg = event.getMessage(); //The chat message the player sent
 		Player sender = event.getPlayer(); //The player who sent the chat message
 		
 		if(msg.toUpperCase().equals(msg) && (msg.length() >= config.getInt("msg.minmumlength")) && !msg.contains("...") && !msg.contains("-") && !msg.contains("^") && !sender.hasPermission("nocaps.caps")) { //detect if msg is full caps on a shitty way
-			event.setMessage(msg.substring(0,1).toUpperCase()+msg.substring(1).toLowerCase());
-			sender.sendMessage(config.getString("msg.caps"));
+			event.setMessage(msg.substring(0,1).toUpperCase()+msg.substring(1).toLowerCase()); //convert full caps into lowercase with first letter capitalised
+			sender.sendMessage(config.getString("msg.caps")); //send config message
 			if(config.getBoolean("command.enableexecution")) { //if commend execution is enabled
-			String command = config.getString("command.executeoncaps").replaceAll("<sender>" , sender.getDisplayName());
+			String command = config.getString("command.executeoncaps").replaceAll("<sender>" , sender.getDisplayName()); //construct the command
 			getServer().dispatchCommand(Bukkit.getConsoleSender(), command); //execute command
 			}
 			
